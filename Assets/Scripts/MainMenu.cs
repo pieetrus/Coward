4671dd10bdcd4033a66f12693255e5ea072ec60e
kubi <pietrusjakub@gmail.com>
2020-04-09 13:38:59 +0200

    Get Game Version From Develop

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public Text highscoreText;
+    [SerializeField]
+    private TextMeshProUGUI highscoreText = null;
     // Start is called before the first frame update
     void Start()
     {
         highscoreText.text = "Highscore : " + ((int)PlayerPrefs.GetFloat("Highscore")).ToString();
     }
 
-    public void ToGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
         
 }

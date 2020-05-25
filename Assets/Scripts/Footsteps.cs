28589383b95bac1de1065b137fc8391b9d8bfa94
wek98 <57508778+wek98@users.noreply.github.com>
2020-05-25 20:03:05 +0200

    spatial sound test
    added first spatialized sounds

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections;
+
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour   
+{
+    CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cc.isGrounded == true && cc.velocity.magnitude> 2 && GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().volume = Random.Range(0.8f, 1);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
